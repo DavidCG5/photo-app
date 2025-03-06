82d805e880bbbdac47e753d01b8506c8d00dd1b5
DavidCG5 <98667227+DavidCG5@users.noreply.github.com>
2025-03-05 20:17:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-app',
+  webDir: 'www'
+};
+
+export default config;
